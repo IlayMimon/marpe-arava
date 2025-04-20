e53a7d8927c9803445de4d9f421bcb25d63757d9
לירז פנחסי <manraz487@gmail.com>
2025-04-20 19:00:15 +0300

    fix isuss

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {HashRouter, Route, Routes} from "react-router-dom"
+import { HashRouter, Route, Routes } from "react-router-dom"
 
 function App() {
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './scss/main.scss'
 import App from './App.tsx'
 
