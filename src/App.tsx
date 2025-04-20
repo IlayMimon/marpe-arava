4393f9e9082d4d2eccb2b4f7a82a2dd749e6862e
לירז פנחסי <manraz487@gmail.com>
2025-04-20 16:53:21 +0300

    fix isuss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import {HashRouter,Route,Routes} from "react-router-dom"
-import HomePage from './pages/HomePage'
 
 function App() {
 
   return (
    <HashRouter>
        <Routes>
-           <Route index element={<HomePage/>} />
            <Route path="*" element={<div>404</div>}/>
        </Routes>
    </HashRouter>
