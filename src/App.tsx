0742e35c75933bb96bcdf2b2d0f96baea4a1b543
ilay maimon <ilaymimon@gmail.com>
2025-04-21 13:46:25 +0300

    add folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { HashRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
 
 function App() {
-
     return (
         <HashRouter>
             <Routes>
+                <Route path="/" element={<HomePage />} />
                 <Route path="*" element={<div>404</div>} />
             </Routes>
         </HashRouter>
