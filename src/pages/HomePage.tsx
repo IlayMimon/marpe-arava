f4484f9ef3924747929936ff459ed80c625baa1a
Noam Cohen <noamcohen@h-MacBook-Air-sl-Noam.local>
2025-04-22 14:58:50 +0300

    sidebar ready for pr

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,16 @@
-const HomePage = () => {
-    return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-            <p>This is the main page of the application.</p>
-        </div>
-    );
+import SidebarNavigation from '../components/sidebarNavigation';
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <div className="sidebar-navigation">
+        <SidebarNavigation />
+      </div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main page of the application.</p>
+    </div>
+  );
 };
 
 export default HomePage;
