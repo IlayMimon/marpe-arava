30e148ad1397e51cf651c49213e7c3f52af46c1b
Ilay Maimon <ilaymimon@gmail.com>
2025-04-27 15:05:59 +0300

    cr

--- src/components/sidebarNavigation.tsx
+++ src/components/sidebarNavigation.tsx
@@ -6,7 +6,7 @@ const { Sider } = Layout;
 
 const SidebarNavigation: React.FC = () => {
   return (
-    <Sider>
+    <Sider className="sidebar-navigation">
       <img className="sidebar-navigation--image" src={marpeLogo} />
       <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
         <Menu.Item key="1" icon={<TbBus />}></Menu.Item>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,14 @@
-import SidebarNavigation from '../components/sidebarNavigation';
-import React from 'react';
+import SidebarNavigation from "../components/sidebarNavigation";
+import React from "react";
 
 const HomePage: React.FC = () => {
   return (
-    <div>
-      <div className="sidebar-navigation">
-        <SidebarNavigation />
+    <div className="home-page">
+      <SidebarNavigation />
+      <div className="home-page__content">
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the main page of the application.</p>
       </div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main page of the application.</p>
     </div>
   );
 };
