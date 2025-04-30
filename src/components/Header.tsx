df99d75b5a83c43fe1b8ee6b0f8fa22826701e09
לירז פנחסי <manraz487@gmail.com>
2025-04-30 16:35:55 +0300

    merge NP

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
+import HeaderDatePicker from "./HeaderDatePicker";
 import Kpies from "./Kpies";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header__right">
-        <div className="header__right__date">date</div>
+        <HeaderDatePicker/>
         <div className="header__right__medic">medic</div>
       </div>
       <div className="header__left">
