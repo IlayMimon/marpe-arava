73dc1081347b5052edbe6b07e1de8e4f986e1e83
לירז פנחסי <manraz487@gmail.com>
2025-05-02 15:39:06 +0300

    save before shut dhown

---
+++ src/components/ShuttleTable/ShuttleTableHeader.tsx
@@ -0,0 +1,21 @@
+import { Flex, Segmented } from "antd";
+import { SizeType } from "antd/es/config-provider/SizeContext";
+import { useState } from "react";
+
+interface ShuttleTableHeaderProps {}
+export default function NewPatientFormContent({}: ShuttleTableHeaderProps) {
+  const [size, setSize] = useState<SizeType>("middle");
+  return (
+    <div className="shuttle-table-header">
+      <Flex gap="large" align="flex-start" vertical>
+        <Segmented
+        dir="ltr"
+          className="shuttle-table-header__segmented"
+          options={["הלוך", "חזור"]}
+          value={size}
+          onChange={(value) => setSize(value as SizeType)}
+        />
+      </Flex>
+    </div>
+  );
+}
