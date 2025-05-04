4ea7e15d8dbe2071452849f9499ee58ed341d59d
Idan Heskel <idan7856@gmail.com>
2025-05-04 19:47:13 +0300

    added proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => {
+          if (path.toLowerCase().startsWith('/_api/web')) {
+            return path;
+          }
+
+          return `/_api/web/${path.startsWith('/') ? path.slice(1) : path}`;
+        },
+      },
+    },
+  },
+});
