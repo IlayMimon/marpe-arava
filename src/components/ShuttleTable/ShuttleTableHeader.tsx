fb80d10fea5d6766b9fab23879145299b3212c49
eliraz487 <manraz487@gmail.com>
2025-05-06 14:58:31 +0300

    merge np

--- src/components/ShuttleTable/ShuttleTableHeader.tsx
+++ src/components/ShuttleTable/ShuttleTableHeader.tsx
@@ -1,21 +1,24 @@
-import { Flex, Segmented } from "antd";
+import { ConfigProvider, Flex, Segmented } from "antd";
 import { SizeType } from "antd/es/config-provider/SizeContext";
 import { useState } from "react";
 
 interface ShuttleTableHeaderProps {}
 export default function NewPatientFormContent({}: ShuttleTableHeaderProps) {
-  const [size, setSize] = useState<SizeType>("middle");
+  const [size, setSize] = useState<SizeType>("large");
   return (
     <div className="shuttle-table-header">
+      <ConfigProvider direction="rtl">
       <Flex gap="large" align="flex-start" vertical>
         <Segmented
         dir="ltr"
           className="shuttle-table-header__segmented"
-          options={["הלוך", "חזור"]}
+          options={["הלוך", "חזור"]} 
+          block
           value={size}
           onChange={(value) => setSize(value as SizeType)}
         />
       </Flex>
+      </ConfigProvider>
     </div>
   );
 }
