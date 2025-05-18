d5ce51603d253aa4b5279dbcf39d72034bbf29a6
Idan Hascal <idan7856@gmail.com>
2025-05-19 00:51:21 +0300

    reverted App.tsx changes meant for dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,12 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import DriverOrganization from './components/DriverOrganization/DriverOrganization';
-import DriverData from './types/DriverOrganizationTypes';
-import { Modal as AntModal } from 'antd';
-import { useState } from 'react';
-
-const driverData: [DriverData, DriverData, DriverData] = [
-  {
-    distance: 100,
-    name: "test",
-    paths: [
-      {
-        pathId: 1003,
-        stations: [
-          {
-            stationName: "מחנה שיזפון",
-            arrivalTime: "07:30",
-            participants: ["יונתן", "רוברט"],
-          },
-          {
-            stationName: "משהו",
-            arrivalTime: "08:30",
-            participants: ["יונתן", "רוברט"],
-          },
-          {
-            stationName: 'מרפ"א ערבה',
-            arrivalTime: "09:30",
-          },
-        ],
-      },
-      {
-        pathId: 1004,
-        stations: [
-          {
-            stationName: 'מרפ"א ערבה',
-            arrivalTime: "09:00",
-          },
-          {
-            stationName: "מחנה אש",
-            arrivalTime: "09:30",
-            participants: ["יונתן", "רוברט"],
-          },
-          {
-            stationName: "משהו",
-            arrivalTime: "10:00",
-            participants: ["יונתן", "רוברט"],
-          },
-          {
-            stationName: 'מרפ"א ערבה',
-            arrivalTime: "10:15",
-          },
-        ],
-      },
-      { startTime: "10:30", endTime: "11:30", title: "הפסקת צהריים" },
-      {
-        pathId: 1005,
-        stations: [
-          {
-            stationName: "מחנה אש",
-            arrivalTime: "13:30",
-            participants: ["john", "doe"],
-          },
-          {
-            stationName: "משהו",
-            arrivalTime: "16:00",
-            participants: ["john", "doe"],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    distance: 300,
-    paths: [],
-  },
-  { distance: 500, paths: [] },
-];
+import { HashRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route
-          path="*"
-          element={
-            <>
-              <AntModal
-                open={isModalOpen}
-                footer={null}
-                closeIcon={false}
-                onCancel={() => setIsModalOpen(false)}
-                width="131.5rem"
-                centered
-                className="driver-organization__modal"
-              >
-                <DriverOrganization paramedic="שקד דנון" data={driverData} chosenDate={new Date()} />
-              </AntModal>
-              <button onClick={() => setIsModalOpen(true)}>open</button>
-            </>
-          }
-        />
+        <Route path="*" element={<div>404</div>} />
       </Routes>
     </HashRouter>
   );
