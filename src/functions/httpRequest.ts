6e05ea1d1e4a5ae9f2f933bce86f79a64216a4e3
Idan Heskel <idan7856@gmail.com>
2025-05-19 14:31:13 +0300

    fixed httpRequest

--- src/functions/httpRequest.ts
+++ src/functions/httpRequest.ts
@@ -1,7 +1,12 @@
-import axios, { Method } from 'axios';
+import axios, { AxiosRequestConfig, Method, RawAxiosRequestHeaders } from 'axios';
 // should only be used directly for non-GET request methods.
-const httpRequest = async <T, B = unknown>(url: string, method: Method = 'GET', body?: B | undefined) => {
-  const config = { url, method, body };
+const httpRequest = async <T, B = unknown>(
+  url: string,
+  method: Method = 'GET',
+  data?: B | undefined,
+  headers?: RawAxiosRequestHeaders
+) => {
+  const config: AxiosRequestConfig = { url, method, data, headers };
 
   return await axios<T>(config).then((response) => {
     return response.data;
