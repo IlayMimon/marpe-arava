bee4f2663e425dbbfa49b685addd463d17069655
Ilay Maimon <ilaymimon@gmail.com>
2025-05-20 23:34:17 +0300

    cr

--- src/components/ShuttleTable/ShuttleTableHeader.tsx
+++ src/components/ShuttleTable/ShuttleTableHeader.tsx
@@ -1,6 +1,5 @@
-import { ConfigProvider, Flex, Segmented, Space, Input } from 'antd';
-const { Search } = Input;
-import { TripDirection } from '../HomeScreenBody';
+import { ConfigProvider, Flex, Segmented } from "antd";
+import { TripDirection } from "../HomeScreenBody";
 interface IShuttleTableHeaderProps {
   tripDirection: TripDirection;
   handleChange: (direction: TripDirection) => void;
@@ -17,29 +16,22 @@ export default function ShuttleTableHeader({
           gap="large"
           align="flex-start"
           style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            width: '100%',
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: "100%",
           }}
         >
           <Segmented
             dir="ltr"
             className="shuttle-table-header__segmented"
             options={[
-              { label: 'הלוך', value: 'outbound' },
-              { label: 'חזור', value: 'return' },
+              { label: "הלוך", value: "outbound" },
+              { label: "חזור", value: "return" },
             ]}
             block
             value={tripDirection}
             onChange={(direction) => handleChange(direction as TripDirection)}
           />
-          {/* <Space direction="vertical">
-            <Search
-              placeholder="input search text"
-              onSearch={() => {}}
-              style={{ width: 200 }}
-            />
-          </Space> */}
         </Flex>
       </ConfigProvider>
     </div>
