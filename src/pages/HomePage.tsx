8bf49f8ac951d09abb2142661a978dae90a32f74
tal <talgranot100@gmail.com>
2025-05-22 18:28:23 +0300

    added edit patiend modal

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import EditPatientModal, { PatientFormValues } from "../components/EditPatientModal";
 import Header from "../components/Header";
 import HomeScreenBody from "../components/HomeScreenBody";
 import SidebarNavigation from "../components/sidebarNavigation";
@@ -14,7 +15,11 @@ const HomePage: React.FC = () => {
           <Header />
         </header>
         <div className="home-page__content__body">
-          <HomeScreenBody />
+          <EditPatientModal isOpen={true} onClose={function (): void {
+            throw new Error("Function not implemented.");
+          } } onSubmit={function (values: PatientFormValues): void {
+            throw new Error("Function not implemented.");
+          } } />
         </div>
       </div>
     </div>
