8c39bd30cae189021d387a40f0d6cdeb06a25ff2
לירז פנחסי <manraz487@gmail.com>
2025-06-24 13:12:08 +0300

    fix scss and get the area

--- src/hooks/data/useGetShuttles.ts
+++ src/hooks/data/useGetShuttles.ts
@@ -3,12 +3,19 @@ import filterByToday from "../../functions/filterByToday";
 import { SharepointQueryResultArray } from "../../types/spFetchTypes";
 import { useQueryFetchRequest } from "../useQueryFetch";
 
+export type Station = {
+  Title: string;
+  Area: string;
+};
+
 export type Shuttle = {
   ID: number;
   Title: string;
   StartTime: Date;
   ArrivalTime: Date;
   Details: string;
+  stations: string[];
+  area: string;
   RequestsId: { results: number[] };
   DriverId: number | null;
   totalDistance: number;
@@ -20,13 +27,39 @@ export type Shuttle = {
 
 const useGetShuttles = () => {
   const date = useHomePageContext().selectedDate;
-  const { data, refetch } = useQueryFetchRequest<SharepointQueryResultArray<Shuttle>>(
+
+  const { data: shuttlesData, refetch: refetchShuttles } = useQueryFetchRequest<SharepointQueryResultArray<Shuttle>>(
     `/_api/web/lists/getbytitle('shuttles')/items?$select=ID,Title,StartTime,ArrivalTime,Details,RequestsId,DriverId,totalDistance,driverData/ID,driverData/Title&$expand=driverData&${filterByToday(date, "StartTime")}`
   );
 
-  const shuttles = data?.d.results;
+  const { data: stationsData, refetch: refetchStations } = useQueryFetchRequest<SharepointQueryResultArray<Station>>(
+    `/_api/web/lists/getbytitle('stations')/items?$select=Title,Area`
+  );
 
-  return { shuttles, refetch };
+  const shuttles = shuttlesData?.d.results;
+  const stationsList = stationsData?.d.results;
+
+  const stationMap: { [stationName: string]: string } = {};
+  stationsList?.forEach((station) => {
+    stationMap[station.Title.trim()] = station.Area.trim();
+  });
+
+  shuttles?.forEach((shuttle) => {
+    const regex = /([^:]+): \d{2}:\d{2}/g;
+    const matches = shuttle.Details.matchAll(regex);
+    const stations: string[] = [];
+
+    for (const match of matches) {
+      stations.push(match[1].trim());
+    }
+
+    shuttle.stations = stations;
+
+    const lastStation = stations[stations.length - 1] ;
+    shuttle.area = stationMap[lastStation] || "";
+  });
+
+  return { shuttles, refetch: refetchShuttles };
 };
 
 export default useGetShuttles;
