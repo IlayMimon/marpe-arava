0f6ec04c2b80ff66d21833b46c3ccd3402afdbd6
yehonatan danino <daninoyehonatan43@gmail.com>
2025-09-08 13:05:28 +0300

    shuttleTimeLimit

--- src/automation/createSPItems/createSPItems.ts
+++ src/automation/createSPItems/createSPItems.ts
@@ -1,4 +1,4 @@
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 import { postToSharepoint } from "../../functions/postToSharepoint";
@@ -17,24 +17,34 @@ dayjs.extend(timezone);
 // הגדרת timezone ברירת מחדל
 dayjs.tz.setDefault("Asia/Jerusalem");
 
-const getPossibleTravelTimeAddition = (firstArrival: string | undefined, endTime: dayjs.Dayjs | undefined): number => {
+
+const combineDateAndTime = (datePart: Dayjs | undefined, timePart: Dayjs): Dayjs => {
+  if (!datePart) return timePart;
+  return datePart
+    .set("hour", timePart.hour())
+    .set("minute", timePart.minute())
+    .set("second", timePart.second())
+    .set("millisecond", timePart.millisecond());
+};
+
+
+const getPossibleTravelTimeAddition = (startTime: Dayjs | undefined, endTime: Dayjs | undefined): number => {
   // לוגיקה לחישוב תוספת זמן הנסיעה האפשרית
-  const startTime = dayjs.tz(firstArrival, "HH:mm", "Asia/Jerusalem");
-  const sixThirty = dayjs("06:30", "HH:mm");
-  const lunchStart = dayjs("11:30", "HH:mm");
-  const lunchEnd = dayjs("12:30", "HH:mm");
-  console.log('hourMinute', startTime.format("HH:mm"), endTime?.format("HH:mm"));
+  const sixThirty = combineDateAndTime(startTime, dayjs("06:30", "HH:mm"));
+  const lunchStart = combineDateAndTime(startTime, dayjs("11:30", "HH:mm"));
+  const lunchEnd = combineDateAndTime(startTime, dayjs("12:30", "HH:mm"));
+  
 
 
-  if(startTime.isAfter(sixThirty) || endTime?.isBefore(lunchStart) || startTime.isAfter(lunchEnd)) {
+  if(startTime?.isAfter(sixThirty) && endTime?.isBefore(lunchStart) && startTime?.isAfter(lunchEnd)) {
     return 0
-  }else if (startTime.isBefore(sixThirty)) {
+  }else if (startTime?.isBefore(sixThirty)) {
     return sixThirty.diff(startTime, "minute");     // כמה דקות נשארו
   } else {
     // הפרש דקות עד סיום ב-11:30
-    const shiftToBeforeLunch = lunchStart.diff(endTime, "minute"); // שלילי או 0
+    const shiftToBeforeLunch = lunchStart.diff(endTime, "minute"); 
     // הפרש דקות עד התחלה ב-12:30
-    const shiftToAfterLunch = lunchEnd.diff(startTime, "minute");  // חיובי או 0
+    const shiftToAfterLunch = lunchEnd.diff(startTime, "minute"); 
 
     if (Math.abs(shiftToBeforeLunch) <= Math.abs(shiftToAfterLunch)) {
     return shiftToBeforeLunch; // להזיז אחורה
@@ -49,42 +59,18 @@ const getPossibleTravelTimeAddition = (firstArrival: string | undefined, endTime
 export async function creatSPItems(shuttleGroups: enrichShuttleGroup[]) {
     const SPSuttles: SPShuttle[] = [];
   for (const group of shuttleGroups) {
-    ShuttleDetailsPerRequestIds = []; // איפוס
-
-    // יצירת ShuttleDetailsPerRequest לפי הבקשות בקבוצה
-    // for (const request of group.value) {
-    //   // מציאת התחנה המתאימה לבקשה הנוכחית
-    //   const stationMatch = group.stations?.find(
-    //     (s) => s.station.id === request.stationId
-    //   );
-
-    //   if (!stationMatch) continue;
-
-    //   // יצירת ShuttleDetailsPerRequest
-    //   const pickupTime = stationMatch.arrivalTime;
-    //   const arrivalTime = group.arrivalTime.toISOString();
-    //   const res = await postToSharepoint<SharepointQueryResult<SPShuttleDetailsPerRequest>>(`_api/web/lists/getbytitle('ShuttleDetailsPerRequest')/items`, {
-    //       PickupTime: pickupTime,
-    //       ArrivalTime: arrivalTime,
-    //       RequestId: request.shuttlesRequestId,
-    //     },)
-      
-
     
-    //   const createdItemId = res.data.d.Id;
-    //   ShuttleDetailsPerRequestIds.push({ Id: createdItemId });
-    // }
-
     // חישוב סך המרחקים
     const stationsToSum = group.stations ?? [];
     TotalDistance = stationsToSum.reduce(
       (sum, s) => sum + (s.station.distance || 0),
       0
     );
+
+
     const firstArrival = group.stations?.[0]?.arrivalTime;
-    console.log(firstArrival)
     const possibleTravelTimeAddition = getPossibleTravelTimeAddition(firstArrival, group.arrivalTime);
-    console.log(possibleTravelTimeAddition)
+
     // פורמט טקסט של התחנות
     const stationsText = stationsToSum
       .map((s) =>
@@ -104,7 +90,7 @@ export async function creatSPItems(shuttleGroups: enrichShuttleGroup[]) {
       }
       SPSuttles.push(SPSuttle);
       
-    }
+  }
     const shuttlesWithDrivers = assignShuttlesToDrivers(SPSuttles, initDrivers(3))
     for (const shuttle of shuttlesWithDrivers) {
       await postToSharepoint<SharepointQueryResult<SPShuttle>>(`_api/web/lists/getbytitle('Shuttles')/items`, shuttle);
