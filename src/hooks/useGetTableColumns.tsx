c49be7cd02ecaf189c70390215027814bc64e195
mayzu <mayamayzus@gmail.com>
2025-09-08 19:35:45 +0300

    made areas into a new working list according to EEFYOON

--- src/hooks/useGetTableColumns.tsx
+++ src/hooks/useGetTableColumns.tsx
@@ -11,7 +11,7 @@ const formatTime = (value: dayjs.Dayjs | undefined) => {
 };
 
 const useGetTableColumns = (tripDirection: TripDirection) => {
-  const areas = Array.from(new Set(useGetStations()?.map((station) => station.Area)));
+  const areas = Array.from(new Set(useGetStations()?.map((station) => `${station.Area}${station.StationOrder}`)));
 
   const directionColumns: TableColumn<TableRow>[] =
     tripDirection === "outbound"
