c0c85329f551f69979aeb3cf089bdb8417b0a262
GhalFriedman <ghalfriedman@gmail.com>
2026-02-12 15:21:27 +0200

    wip sms changes and changing whatapp to sms

--- src/functions/sendWhatsAppMessages.ts
+++ src/functions/sendWhatsAppMessages.ts
@@ -1,5 +1,41 @@
 import { TableRow } from "../components/Table/TableTypes";
 
+
+// https://cellsms.cellcom.co.il/SmsGate2.asmx/SendSmsEx?username=user&password=pass&target=0521234567&source=1234&message=Test&pushUrl=&validity=1&replace=false&immediate=false&isBinary=false&deliveryReceipt=true&maxSegments=0
+
+
+const sendSmsMessages = async (tableData: TableRow[]) => {
+  const messagesInfo = tableData
+    .filter((r) => r.status === "שובץ")
+    .map((row) => ({
+      username: 'marpearava',
+      password: 'Marpearava123!',
+      target: `972${row.phone.slice(1)}`, // Remove the leading '0' and add country code
+      time: row.pickupTime,
+      name: row.fullName,
+      station: row.station,
+      driver: row.driver,
+    }));
+
+  const res = await fetch("https://cellsms.cellcom.co.il/SmsGate2.asmx/SendSmsEx", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(messagesInfo),
+  });
+
+  if (res.ok) {
+    alert("✅ Message sent!");
+  } else {
+    alert("❌ Failed to send message");
+  }
+};
+
+export default sendWhatsMessages;
+
+
+/*
+import { TableRow } from "../components/Table/TableTypes";
+
 const sendWhatsMessages = async (tableData: TableRow[]) => {
   const messagesInfo = tableData
     .filter((r) => r.status === "שובץ")
@@ -25,3 +61,4 @@ const sendWhatsMessages = async (tableData: TableRow[]) => {
 };
 
 export default sendWhatsMessages;
+*/
